2b970afea8207a183f0c263d6b59cb691f4be6d8
enireves <enirevescode@gmail.com>
2023-10-07 21:31:09 +0200

    fetch data with watch

--- src/components/RickMortyCards.vue
+++ src/components/RickMortyCards.vue
@@ -1,17 +1,36 @@
 <script setup>
 import axios from "axios"
+import {ref, watch} from "vue"
 
+//stockage du state
+const characters = ref(null)
 
-// Récup data de l API
+//pagination
+const page = ref(0)
 
+// Récup data de l API + limitation à 8 du nbre de cards/page
 const response = await axios.get("https://rickandmortyapi.com/api/character?limit=8")
-console.log(response.data.length)
+characters.value = response.data
+console.log ("characters.value",characters.value.results)
+console.log( "type de data",typeof response)
+console.log( " length",response.data.length)
+
+
+//Mise en marche des btn "next" et "back"
+watch(page, async () =>{
+    const res = await axios.get(`https://rickandmortyapi.com/api/character?limit=8&offset=${page.value*8}`)
+    characters.value = res.data
+})
+
+
 </script>
 
 <template>
     <div>
         
         <h1>Rick and Morty</h1>
-        <h2>{{ response.data.length }}</h2>
+        <h2>{{ characters }}</h2>
+        <button @click="page++">Next</button>
+        <button @click="page--">Back</button>
     </div>
 </template>
